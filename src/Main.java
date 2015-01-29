ec0b783f700c779cd1017bd6bc9f0cb4a9873479
Larissa Laich <la8895@web.de>
2015-01-29 17:07:01 +0100

    insertClient implemented, new pstmt

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
-import java.sql.Connection;
 import java.sql.SQLException;
 
 import model.Client;
 import controller.database.ClientDataBaseController;
 import controller.database.DataBaseConnection;
-import controller.database.DataBaseTables;
 
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("test");
+
 		try {
 			DataBaseConnection.getDataBaseConnection();
 			
@@ -16,7 +16,9 @@ public class Main {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		Client test = new Client.ClientBuilder("Test").email("123").createClient();
+
+		Client test = new Client.ClientBuilder("Fre").email("123").createClient();
+		System.out.println(ClientDataBaseController.loadClient(test.getId()).getName());
 	}
 	
 }
--- src/controller/database/ClientDataBaseController.java
+++ src/controller/database/ClientDataBaseController.java
@@ -5,11 +5,11 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+
 import model.Client;
 
 public class ClientDataBaseController {
 
-	
 	/**
 	 * inserts Client to database
 	 * 
@@ -21,36 +21,62 @@ public class ClientDataBaseController {
 		int keyID = -1;
 		try {
 			dataBaseConnection = DataBaseConnection.getDataBaseConnection();
-		
-		if(dataBaseConnection != null){
-			String insertQuery = String.format(
-					"INSERT INTO Client VALUES(null,'%s' , '%s')",client.getName(), client.getEmail()
-					);
-			//TBD if(employee/task list is not empty => add links in database)
-			PreparedStatement pstmt = dataBaseConnection.prepareStatement(
-					insertQuery, Statement.RETURN_GENERATED_KEYS);
-			pstmt.executeUpdate();
-			ResultSet keys = pstmt.getGeneratedKeys();
-			//if the client is inserted, the id in the database is returned here
-			if (keys.next()) {
-				keyID = keys.getInt(1);
+
+			if (dataBaseConnection != null) {
+
+				String insertQuery = "INSERT INTO Client (id, Name, Email) VALUES (null,?,?)";
+				// TBD if(employee/task list is not empty => add links in
+				// database)
+				PreparedStatement pstmt = dataBaseConnection.prepareStatement(
+						insertQuery, Statement.RETURN_GENERATED_KEYS);
+				pstmt.setString(1, client.getName());
+				pstmt.setString(2, client.getEmail());
+				pstmt.executeUpdate();
+				ResultSet keys = pstmt.getGeneratedKeys();
+				// if the client is inserted, the id in the database is returned
+				// here
+				if (keys.next()) {
+					keyID = keys.getInt(1);
+				}
+
 			}
-		
-		}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return keyID;
 	}
-	
+
 	/**
 	 * loads Client from database
 	 * 
 	 * @return specific client for id (which is saved in database)
 	 */
 	public static Client loadClient(int clientId) {
-		//TBD
+		Connection dataBaseConnection;
+		try {
+			dataBaseConnection = DataBaseConnection.getDataBaseConnection();
+
+			if (dataBaseConnection != null) {
+				String selectSQL = "SELECT * FROM Client WHERE id = ?";
+				PreparedStatement pstmt = dataBaseConnection
+						.prepareStatement(selectSQL);
+				pstmt.setInt(1, clientId);
+				// execute the Query
+				ResultSet rs = pstmt.executeQuery();
+				Client client = null;
+				if (rs.next()) {
+					// TBD also read tasks and employee links here => from link
+					// dataBaseController
+					client = new Client.ClientBuilder(rs.getString("Name"))
+							.id(rs.getInt("Id")).email(rs.getString("Email"))
+							.createClient();
+				}
+				return client;
+			}
+		} catch (Exception e) {
+				e.printStackTrace();
+		}
 		return null;
 	}
 }
